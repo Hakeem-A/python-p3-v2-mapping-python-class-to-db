3b0f3b3017fd8d91c449c9a1557de0bb6473547c
Abdi Mohamed <bashanesix@gmail.com>
2025-03-08 12:41:10 +0300

    lab done

--- lib/debug.py
+++ lib/debug.py
@@ -1,9 +1,18 @@
 #!/usr/bin/env python3
 #lib/testing/debug.py
 
+
 from __init__ import CONN, CURSOR
 from department import Department
 
 import ipdb
 
+Department.drop_table()
+Department.create_table()
+
+payroll = Department.create("Payroll", "Building A, 5th Floor")
+print(payroll)  # <Department 1: Payroll, Building A, 5th Floor>
+
+hr = Department.create("Human Resources", "Building C, East Wing")
+print(hr)  # <Department 2: Human Resources, Building C, East Wing>
 ipdb.set_trace()
